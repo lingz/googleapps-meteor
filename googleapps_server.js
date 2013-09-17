ee991c5658372930d2308e94840a31d9491fb1ae
lingz <lz781@nyu.edu>
2013-09-17 18:34:27 +0400

    working now

--- googleapps_server.js
+++ googleapps_server.js
@@ -47,7 +47,7 @@ var getTokens = function (query) {
         code: query.code,
         client_id: config.clientId,
         client_secret: config.secret,
-        redirect_uri: Meteor.absoluteUrl("_oauth/google?close"),
+        redirect_uri: Meteor.absoluteUrl("_oauth/googleApps?close"),
         grant_type: 'authorization_code'
       }});
   } catch (err) {
